cc936bc3262e8c04af743b5c42799161614792ec
ik <jeongik11029@naver.com>
2024-08-07 20:18:42 +0900

    정익 ScoreManagement makeKey if문으로 이동 진짜 마지막

--- src/camp/model/Score.java
+++ src/camp/model/Score.java
@@ -8,9 +8,9 @@ import java.util.Map;
 
 public class Score {
 
-    private String subjectId;  // 어떤 과목의 점수
-    private String studentId;  // 어떤 학생의 점수
-    private final GradeConvertor gradeConvertor;
+    private String subjectId;  // 과목의 점수
+    private String studentId;  // 학생의 점수
+    private final GradeConvertor gradeConvertor; // 점수 -> 등급 변환기
     private final Map<Integer, Integer> scoreMap; // 회차에 따른 점수
     private final Map<Integer, Grade> gradeMap; // 회차에 따른 등급
 
